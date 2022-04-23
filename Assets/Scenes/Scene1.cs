7a31df7315011d22347348f802943515b416f62f
IlanRepiso <101400193+IlanRepiso@users.noreply.github.com>
2022-04-23 12:43:29 -0300

    Hasta el video 3

---
+++ Assets/Scenes/Scene1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene1 : MonoBehaviour
+{
+    public int edad;
+    public Text miTexto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edad < 18)
+        {
+            miTexto.text = "Es menor de edad";
+            Debug.Log("Es menor de edad"); 
+        }
+        else
+        {
+            miTexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
